94e4f4e7ef83e56ff3c3ad53ca0bcee6df73e1ce
Ryan M. Cosheril <Ryan.M.Cosheril@students.ittralee.ie>
2019-11-04 12:30:48 +0000

    Initial commit

---
+++ Tests/MatrixGraphTest.java
@@ -0,0 +1,88 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatrixGraphTest {
+
+    private Graph directedGraph;
+    private Graph undirectedGraph;
+
+    @BeforeEach
+    void setUp()
+    {
+        directedGraph = new MatrixGraph(5, true);
+        undirectedGraph = new MatrixGraph(5, false);
+    }
+
+    @Test
+    void isEdge() {
+
+    }
+
+    @Test
+    void insert() {
+        Edge edge = new Edge(0, 1);
+        directedGraph.insert(edge);
+
+        boolean expected = true;
+        boolean actual = directedGraph.isEdge(0, 1);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void insertUndirected() {
+        Edge edge = new Edge(0, 1);
+        undirectedGraph.insert(edge);
+
+        boolean expected = true;
+        boolean actual = undirectedGraph.isEdge(0, 1) && undirectedGraph.isEdge(1, 0);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void insertFail() {
+        boolean expected = false;
+        boolean actual = directedGraph.isEdge(0, 1);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void remove() {
+        Edge edge = new Edge(0, 1);
+        directedGraph.insert(edge);
+        directedGraph.remove(edge);
+
+        boolean expected = true;
+        boolean actual = !directedGraph.isEdge(0, 1);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void removeUndirected() {
+        Edge edge = new Edge(0, 1);
+        directedGraph.insert(edge);
+        directedGraph.remove(edge);
+
+        boolean expected = true;
+        boolean actual = !directedGraph.isEdge(0, 1) && !undirectedGraph.isEdge(1, 0);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void removeFail() {
+        Edge edge = new Edge(0, 1);
+        directedGraph.insert(edge);
+        directedGraph.remove(edge);
+
+        boolean expected = true;
+        boolean actual = !directedGraph.isEdge(0, 1);
+
+        assertEquals(expected, actual);
+    }
+}
